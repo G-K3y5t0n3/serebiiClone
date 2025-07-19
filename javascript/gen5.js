ab775b0963050dd0bea9f39c8e4eae15f33325ee
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-07-19 00:32:05 -0600

    final project progress

---
+++ javascript/gen5.js
@@ -0,0 +1,13 @@
+const snivy = {
+id: 495,
+name: "Snivy",
+classification: "Grass Snake",
+type: "Grass",
+pokedex_entry: "It is very intelligent and calm, using their vines more adeptly than its hands. They photosynthesize by bathing their tails in sunlight, which increases its movement speed. When they are not feeling well, their tails droop.",
+strengths: ["Ground", "Rock", "Water"],
+weaknesses: ["Bug", "Fire", "Flying", "Ice", "Poison"],
+stats: "images/snivyStats.jpg",
+sprite: "images/snivy.jpg",
+evolution_line: "snivyEvoLine",
+};
+
